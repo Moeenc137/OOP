6f6de951d955d8227b2db8d20d56c1aa47738184
Moeenc137 <moeenc137@gmail.com>
2025-12-28 01:48:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,17 @@ karanArjun.__proto__ = employee;
 //blueprint to cars , it has some functions and the objects which is based on the
 //specific class will inheritly have all the functions which are described in class.
 
+//constructor keyword:
+//constructor is a reserved keyword which is created automatically when an object made through
+//class, it is basically a function we can also set values to it if we want to add function to
+//an object at the time of initializing the object.
+
 class toyotaCar {
+  constructor(brand, milage) {
+    console.log("creating an object");
+    this.brand = brand;
+    this.milage = milage;
+  }
   start() {
     startCar: "The car is on";
   }
@@ -47,7 +57,39 @@ class toyotaCar {
     this.brand = brand;
   }
 }
-let fortuner = new toyotaCar();
-fortuner.setBrand("Toyota");
-let civic = new toyotaCar();
-civic.setBrand("Honda");
+let fortuner = new toyotaCar("fortuner", 10);
+// fortuner.setBrand("Toyota");
+console.log(fortuner);
+let civic = new toyotaCar("civic", 18);
+console.log(civic);
+// civic.setBrand("Honda");
+
+//inheritence:
+//basically inheritence means in programimg is to give all the functions of a
+//class to another class, like a child gets inheritence of parents.
+
+class person {
+  constructor() {
+    this.species = "homo sapiens";
+  }
+  sleep() {
+    sleep: "Human sleeps";
+  }
+  eat() {
+    eat: "Human eats";
+  }
+}
+
+class Engineer extends person {
+  work() {
+    work: "Build something";
+  }
+}
+let Engineer = new Engineer();
+
+class doctor extends person {
+  work() {
+    work: "treats patients";
+  }
+}
+let Engineer = new Engineer();
